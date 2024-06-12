dc30c48960133ce2821a2c5f34a99ac60c422493
Edward He <ehe058@uottawa.ca>
2024-06-12 19:18:49 -0400

    lab 4 failed test commit 2

--- Lab4/newProject/tests/test_calculator.py
+++ Lab4/newProject/tests/test_calculator.py
@@ -9,13 +9,13 @@ class TestCalculator(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(self.calc.add(1, 2), 2)
-        self.assertEqual(self.calc.add(-1, -1), -1)
+        self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(-1, -1), -2)
         self.assertEqual(self.calc.add(-1, 1), 0)
 
     def test_subtract(self):
-        self.assertEqual(self.calc.subtract(2, 1), 1)
-        self.assertEqual(self.calc.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(2, 1), 2)
+        self.assertEqual(self.calc.subtract(-1, -1), 1)
         self.assertEqual(self.calc.subtract(-1, 1), -2)
 
     def test_multiply(self):
